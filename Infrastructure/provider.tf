998910e6590d3147ff1ff56c6507a910703c00fb
Intikhab Khan <intikhab.khan@team.telstra.com>
2022-07-12 00:50:35 +1200

    fix6.

--- Infrastructure/provider.tf
+++ Infrastructure/provider.tf
@@ -1,10 +1,16 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.93.1"
+    }
+  }
+}
+
 provider "azurerm" {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 2.93.1"
     }
    features {}
-    }
 }
-

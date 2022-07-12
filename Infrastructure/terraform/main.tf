b17e6114d8ad5c0f130cf135366f5f0766ed3d38
Intikhab Khan <intikhab.khan@team.telstra.com>
2022-07-13 02:35:53 +1200

    formetting terraform

--- Infrastructure/terraform/main.tf
+++ Infrastructure/terraform/main.tf
@@ -15,9 +15,24 @@ resource "azurerm_kubernetes_cluster" "clearpoint" {
   dns_prefix          = "clearpointaks"
 
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
+    enable_auto_scaling = true
+    name                = "default"
+    node_count          = 2
+    vm_size             = "Standard_D2_v2"
+  }
+
+  auto_scaler_profile {
+    balance_similar_node_groups      = false
+    max_graceful_termination_sec     = "600"
+    scale_down_delay_after_add       = "10m"
+    scale_down_delay_after_delete    = "10s"
+    scale_down_delay_after_failure   = "3m"
+    scale_down_unneeded              = "10m"
+    scale_down_unready               = "20m"
+    scale_down_utilization_threshold = "0.5"
+    scan_interval                    = "10s"
+    skip_nodes_with_local_storage    = true
+    skip_nodes_with_system_pods      = true
   }
 
   identity {
